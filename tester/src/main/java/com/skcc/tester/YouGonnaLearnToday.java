a4d10e821dc133859b7ec7f70f360084dafaa55e
christianbueno <christianjonbueno@me.com>
2019-04-22 15:55:55 -0700

    Added get mapping

--- tester/src/main/java/com/skcc/tester/YouGonnaLearnToday.java
+++ tester/src/main/java/com/skcc/tester/YouGonnaLearnToday.java
@@ -1,5 +1,13 @@
 package com.skcc.tester;
 
-public class YouGonnaLearnToday {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class YouGonnaLearnToday {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "";
+	}
 }
